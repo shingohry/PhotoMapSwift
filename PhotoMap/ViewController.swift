cb4da4bc941a8574b1cfb6a2a0ba24235adea44f
Shingo Hiraya <shingohiraya@gmail.com>
2015-01-05 15:42:46 +0900

    first commit

---
+++ PhotoMap/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  PhotoMap
+//
+//  Created by 平屋真吾 on 2014/11/25.
+//  Copyright (c) 2014年 Shingo Hiraya. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+
+class ViewController: UIViewController {
+    @IBOutlet weak var mapView: MKMapView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.mapView.rotateEnabled = false
+        self.mapView.pitchEnabled = false
+        
+        let centerCoordinate: CLLocationCoordinate2D = CLLocationCoordinate2DMake(35.681382, 139.766084)
+        let initialSpan: MKCoordinateSpan = MKCoordinateSpanMake(0.4, 0.4)
+        let initialRegion: MKCoordinateRegion = MKCoordinateRegionMake(centerCoordinate, initialSpan)
+        self.mapView.setRegion(initialRegion, animated: true)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PhotoMapTests/PhotoMapTests.swift
@@ -0,0 +1,36 @@
+//
+//  PhotoMapTests.swift
+//  PhotoMapTests
+//
+//  Created by 平屋真吾 on 2014/11/25.
+//  Copyright (c) 2014年 Shingo Hiraya. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class PhotoMapTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
