022082b976c617679780e07d0e8436218109c7db
Shingo Hiraya <shingohiraya@gmail.com>
2015-02-23 08:36:47 +0900

    - フォトライブラリの全写真を元にピンを立てる処理を追加 - フォトライブラリのアクセス許可チェック＆許可要求処理を追加

--- PhotoMap/ViewController.swift
+++ PhotoMap/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 import MapKit
-
+import Photos
 
 class ViewController: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
@@ -17,6 +17,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        self.prepareMapView()
+        self.checkAuthorizationStatus()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    private func prepareMapView() {
         self.mapView.rotateEnabled = false
         self.mapView.pitchEnabled = false
         
@@ -25,12 +35,34 @@ class ViewController: UIViewController {
         let initialRegion = MKCoordinateRegion(center: centerCoordinate, span: initialSpan)
         self.mapView.setRegion(initialRegion, animated: true)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private func checkAuthorizationStatus() {
+        let status = PHPhotoLibrary.authorizationStatus()
+        
+        switch status {
+        case .Authorized:
+            self.prepareAnnotations()
+        default:
+            PHPhotoLibrary.requestAuthorization{ status in
+                if status == .Authorized {
+                    self.prepareAnnotations()
+                }
+            }
+        }
     }
-
-
+    
+    private func prepareAnnotations() {
+        let fetchResult = PHAsset.fetchAssetsWithMediaType(PHAssetMediaType.Image, options: nil)
+        fetchResult?.enumerateObjectsUsingBlock ({result, index, stop in
+            
+            if let asset = result as? PHAsset {
+                if let location = asset.location {
+                    let annotation = MKPointAnnotation()
+                    annotation.coordinate = location.coordinate
+                    self.mapView.addAnnotation(annotation)
+                }
+            }
+        })
+    }
 }
 
