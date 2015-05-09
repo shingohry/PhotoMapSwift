1bbd85e17200320af4292390fd36c09d86fdd00c
Shingo Hiraya <shingohiraya@gmail.com>
2015-05-09 17:20:02 +0900

    AssetViewController, ImageCell クラスを追加

---
+++ PhotoMap/ImageCell.swift
@@ -0,0 +1,24 @@
+//
+//  ImageCell.swift
+//  PhotoMap
+//
+//  Created by 平屋真吾 on 2015/05/09.
+//  Copyright (c) 2015年 Shingo Hiraya. All rights reserved.
+//
+
+import UIKit
+
+class ImageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
