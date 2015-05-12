408560653bed77890b6eb637fb9f736ef7e0211d
Shingo Hiraya <shingohiraya@gmail.com>
2015-05-13 02:24:31 +0900

    Storyboard 上の ImageCell に ImageView を追加 ImageCell に assetImageView, assetImage プロパティを 追加

--- PhotoMap/ImageCell.swift
+++ PhotoMap/ImageCell.swift
@@ -9,7 +9,14 @@
 import UIKit
 
 class ImageCell: UITableViewCell {
-
+    @IBOutlet private weak var assetImageView: UIImageView!
+    
+    var assetImage :UIImage? {
+        willSet {
+            self.assetImageView.image = newValue
+        }
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
